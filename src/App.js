a28c0a1db096b6feb84457232d0b695984c0bd58
ponrajvishnu <ponrajsm@gmail.com>
2022-11-14 23:02:38 +0530

    shopify react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import NavBar from './component/NavBar';
+import Header from './component/Header';
+import Rating from '@mui/material/Rating';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  let [count,setCount] = useState(0); // count - statevariable;setCount - statefunction
+  let [toggle,setToggle] = useState(true);
+  let [toggleTwo,setToggleTwo] = useState(true);
+  let [toggleThree,setToggleThree] = useState(true);
+  //let count = 0;
+  return <>
+    <NavBar data={{count,setCount}}/>
+    <Header />
+    <section className="py-5">
+      <div className="container px-4 px-lg-5 mt-5">
+        <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+            <div className="col mb-5">
+              <div className="card h-100">
+                  <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                  <div className="card-body p-4">
+                      <div className="text-center"><h5 className="fw-bolder">Fancy Product</h5> $40.00 </div>
+                  </div>
+                  <div className="rating-section"><Rating name="size-large" defaultValue={2} size="large" /></div>
+                  <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                      <div className="text-center">
+                        { 
+                          toggle ? <button className="btn btn-outline-dark mt-auto" onClick={() =>{ 
+                            setCount(++count); 
+                            setToggle(!toggle)
+                          }}>Add to Cart</button>
+                          :
+                          <button className="btn btn-outline-dark mt-auto" onClick={() =>{ 
+                            setCount(--count); 
+                            setToggle(!toggle)
+                          }}>Remove</button>
+                        }
+                      </div>
+                  </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                  <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                  <div className="card-body p-4">
+                      <div className="text-center"><h5 className="fw-bolder">Diary Product</h5> $50.00 </div>
+                  </div>
+                  <div className="rating-section"><Rating name="half-rating" defaultValue={2.5} precision={0.5} /></div>
+                  <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                      <div className="text-center">
+                       { 
+                          toggleTwo ? <button className="btn btn-outline-dark mt-auto" onClick={() =>{ 
+                            setCount(++count); 
+                            setToggleTwo(!toggleTwo)
+                          }}>Add to Cart</button>
+                          :
+                          <button className="btn btn-outline-dark mt-auto" onClick={() =>{ 
+                            setCount(--count); 
+                            setToggleTwo(!toggleTwo)
+                          }}>Remove</button>
+                        }
+                      </div>
+                  </div>
+              </div>
+            </div>
+            <div className="col mb-5">
+              <div className="card h-100">
+                  <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                  <div className="card-body p-4">
+                      <div className="text-center"><h5 className="fw-bolder">Auto Product</h5> $30.00 </div>
+                  </div>
+                  <div className="rating-section"><Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly /></div>
+                  <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                      <div className="text-center">
+                      { 
+                        toggleThree ? <button className="btn btn-outline-dark mt-auto" onClick={() =>{ 
+                          setCount(++count); 
+                          setToggleThree(!toggleThree)
+                        }}>Add to Cart</button>
+                        :
+                        <button className="btn btn-outline-dark mt-auto" onClick={() =>{ 
+                          setCount(--count); 
+                          setToggleThree(!toggleThree)
+                        }}>Remove</button>
+                      }
+                      </div>
+                  </div>
+              </div>
+            </div>
+        </div>
     </div>
-  );
+  </section>
+  </> 
+  
 }
 
 export default App;
