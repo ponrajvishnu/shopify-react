438f74f2444f172a7e874cdc61adde7ffba954a6
ponrajvishnu <ponrajsm@gmail.com>
2022-11-19 14:03:32 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
               <div className="card h-100">
                   <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
                   <div className="card-body p-4">
-                      <div className="text-center"><h5 className="fw-bolder">Fancy Product</h5> $40.00 - $80.00 </div>
+                      <div className="text-center"><h5 className="fw-bolder">Fancy Product</h5> $40.00 </div>
                   </div>
                   <div className="rating-section"><Rating name="size-small" defaultValue={3} /></div>
                   <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
@@ -48,6 +48,7 @@ function App() {
             </div>
             <div className="col mb-5">
               <div className="card h-100">
+              <div class="badge bg-dark text-white position-absolute" style={{top: "0.5rem", right: "0.5rem"}}>Sale</div>
                   <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
                   <div className="card-body p-4">
                       <div className="text-center"><h5 className="fw-bolder">Special Item</h5> <span class="text-muted text-decoration-line-through">$20.00</span> $18.00 </div>
@@ -72,6 +73,7 @@ function App() {
             </div>
             <div className="col mb-5">
               <div className="card h-100">
+              <div class="badge bg-dark text-white position-absolute" style={{top: "0.5rem", right: "0.5rem"}}>Sale</div>
                   <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
                   <div className="card-body p-4">
                       <div className="text-center"><h5 className="fw-bolder">Auto Product</h5><span class="text-muted text-decoration-line-through">$50.00</span> $25.00 </div>
@@ -120,6 +122,7 @@ function App() {
             </div>
             <div className="col mb-5">
               <div className="card h-100">
+              <div class="badge bg-dark text-white position-absolute" style={{top: "0.5rem", right: "0.5rem"}}>Sale</div>
                   <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
                   <div className="card-body p-4">
                       <div className="text-center"><h5 className="fw-bolder">Sale Item</h5> <span class="text-muted text-decoration-line-through">$30.00</span> $20.00 </div>
@@ -146,7 +149,7 @@ function App() {
               <div className="card h-100">
                   <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
                   <div className="card-body p-4">
-                      <div className="text-center"><h5 className="fw-bolder">Fancy Product</h5> $120.00 - $180.00 </div>
+                      <div className="text-center"><h5 className="fw-bolder">Fancy Product</h5> $120.00 </div>
                   </div>
                   <div className="rating-section"><Rating name="size-medium" defaultValue={3} /></div>
                   <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
@@ -168,6 +171,7 @@ function App() {
             </div>
             <div className="col mb-5">
               <div className="card h-100">
+              <div class="badge bg-dark text-white position-absolute" style={{top: "0.5rem", right: "0.5rem"}}>Sale</div>
                   <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
                   <div className="card-body p-4">
                       <div className="text-center"><h5 className="fw-bolder">Special Item</h5> <span class="text-muted text-decoration-line-through">$20.00</span> $18.00 </div>
